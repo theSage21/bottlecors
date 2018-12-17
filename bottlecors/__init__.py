cd2eabed34de40689cf48f901fbb0c2055027c6d
Arjoonn Sharma <arjoonn.94@gmail.com>
2018-12-17 16:12:52 +0530

    return a simpel response instead of wrapped

--- bottlecors/__init__.py
+++ bottlecors/__init__.py
@@ -2,42 +2,44 @@ import bottle
 
 
 def __cors_dict(origin=None):
-    origin = '*' if origin is None else origin
-    cors_string = 'Origin, Accept , Content-Type, X-Requested-With, X-CSRF-Token'
-    CORS_HEADERS = {'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
-                    'Access-Control-Allow-Headers': cors_string,
-                    'Access-Control-Allow-Credentials': 'true',
-                    'Access-Control-Allow-Origin': origin}
+    origin = "*" if origin is None else origin
+    cors_string = "Origin, Accept , Content-Type, X-Requested-With, X-CSRF-Token"
+    CORS_HEADERS = {
+        "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT, DELETE",
+        "Access-Control-Allow-Headers": cors_string,
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": origin,
+    }
     return CORS_HEADERS
 
 
-def abort(code=500, text='Unknown Error.'):
+def abort(code=500, text="Unknown Error."):
     """ Aborts execution and causes a HTTP error. """
-    origin = bottle.request.headers.get('Origin')
+    origin = bottle.request.headers.get("Origin")
     d = __cors_dict(origin)
-    raise bottle.HTTPError(code, text, **d)
+    return bottle.Response(text, code, headers=d)
 
 
 def add_cors(app, allow_credentials=True):
-    @app.hook('after_request')
+    @app.hook("after_request")
     def add_cors_headers():
-        origin = bottle.request.headers.get('Origin')
+        origin = bottle.request.headers.get("Origin")
         d = __cors_dict(origin)
         bottle.response.headers.update(d)
 
     def docstring_fn(fn, method):
         def newfn():
             doc = fn.__doc__
-            doc = 'No docstring' if doc is None else doc
-            return method + '\n' + doc
+            doc = "No docstring" if doc is None else doc
+            return method + "\n" + doc
+
         return newfn
 
     new_routes = []
     for route in app.routes:
-        if route.method != 'OPTIONS':
-            new_routes.append((route.rule, docstring_fn(route.callback,
-                                                        route.method)))
+        if route.method != "OPTIONS":
+            new_routes.append((route.rule, docstring_fn(route.callback, route.method)))
     for r, fn in new_routes:
-        app.route(r, method=['OPTIONS'])(fn)
+        app.route(r, method=["OPTIONS"])(fn)
 
     return app
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='bottlecors',
-      version='0.32',
+      version='0.33',
       packages=['bottlecors'],
       install_requires=['bottle'],
       python_requires=">=3.6",
