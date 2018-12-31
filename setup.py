f924e2e4b09ba70f88b3f148038c53559033a1dd
Arjoonn Sharma <arjoonn.94@gmail.com>
2018-12-31 13:23:01 +0530

    Docs

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
 
 
-setup(name='bottlecors',
-      version='0.33',
-      packages=['bottlecors'],
-      install_requires=['bottle'],
-      python_requires=">=3.6",
-      zip_safe=False)
+with open('README.md', 'r') as fl:
+    long_desc = fl.read()
+
+
+setup(
+    name="bottlecors",
+    author="Arjoonn Sharma",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    version="0.34",
+    packages=["bottlecors"],
+    install_requires=["bottle"],
+    python_requires=">=3.6",
+    project_urls={"Source": "https://github.com/thesage21/bottlecors"},
+    zip_safe=False,
+)
